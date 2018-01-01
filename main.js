e19a60d94b037d6c8c044db0ad0825dbe9fdc8be
Tim Kharshan <timkharshan@hotmail.com>
2017-12-31 17:00:17 -0800

    positioned in the middle

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ document.addEventListener('DOMContentLoaded', function(){
     let startY = 150;
     setInterval(function(){
         startX = (startX+1) %300;
-        svg1.setAttribute('d', `M ${startX} ${startY} C 0 150, 0 150, 150 300 S 250 400, 150 500 S 60 600, 150 680 S 200 750, 150 800`)
-        svg2.setAttribute('d', `M ${300-startX} ${startY} C 0 150, 0 150, 150 300 S 250 400, 150 500 S 60 600, 150 680 S 200 750, 150 800`)
+        // svg1.setAttribute('d', `M ${startX} ${startY} C 0 150, 0 150, 150 300 S 250 400, 150 500 S 60 600, 150 680 S 200 750, 150 800`)
+        // svg2.setAttribute('d', `M ${300-startX} ${startY} C 0 150, 0 150, 150 300 S 250 400, 150 500 S 60 600, 150 680 S 200 750, 150 800`)
     },10);
     
 
