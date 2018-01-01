cc9081efa024b21fd7901a7ff776385aa4a49afb
Tim Kharshan <timkharshan@hotmail.com>
2017-12-31 17:30:19 -0800

    svg a bit animated, almost proof of concept. I need to figure out how to animate the crests how I want to

--- main.js
+++ main.js
@@ -7,10 +7,20 @@ document.addEventListener('DOMContentLoaded', function(){
 
     let startX = 150;
     let startY = 150;
+    let opacity = 0;
+    let factor = 1;
     setInterval(function(){
-        startX = (startX+1) %300;
-        // svg1.setAttribute('d', `M ${startX} ${startY} C 0 150, 0 150, 150 300 S 250 400, 150 500 S 60 600, 150 680 S 200 750, 150 800`)
-        // svg2.setAttribute('d', `M ${300-startX} ${startY} C 0 150, 0 150, 150 300 S 250 400, 150 500 S 60 600, 150 680 S 200 750, 150 800`)
+        startX = (startX+(factor)) ;
+        opacity = (startX/300);
+        if (startX > 299){
+            factor = -1;
+        } else if (startX < 1){
+            factor = 1;
+        }
+        svg1.setAttribute('d', `M ${startX} ${startY} C 0 150, 0 150, 150 300 S 250 400, 150 500 S 60 600, 150 680 S 200 750, 150 800`)
+        svg1.setAttribute('fill', `rgba(192,192,192,${opacity})`);
+        svg2.setAttribute('fill',`rgba(255,255,0,${1-opacity})`)
+        svg2.setAttribute('d', `M ${300-startX} ${startY} C 0 150, 0 150, 150 300 S 250 400, 150 500 S 60 600, 150 680 S 200 750, 150 800`)
     },10);
     
 
