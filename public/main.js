8b78c4b2d353ca0ef06c7f443630116361720e57
Tim Kharshan <timkharshan@hotmail.com>
2018-01-01 15:34:54 -0800

    seperators and sizeing

--- public/main.js
+++ public/main.js
@@ -18,6 +18,24 @@ projects.push( new Project(
     ['react','css','stuff']
 ));
 
+projects.push(new Project(
+    'http://res.cloudinary.com/flyakite/image/upload/v1514414844/search_rifri5.png',
+    { source: 'http://www.github.com', live: 'heroku.com' },
+    ['rgba(100,100,100,1)'],
+    "FlyaKite",
+    'React, Rails, and Postgres powered EventBrite clone',
+    ['react', 'css', 'stuff']
+));
+
+projects.push(new Project(
+    'http://res.cloudinary.com/flyakite/image/upload/v1514414844/search_rifri5.png',
+    { source: 'http://www.github.com', live: 'heroku.com' },
+    ['rgba(100,100,100,1)'],
+    "FlyaKite",
+    'React, Rails, and Postgres powered EventBrite clone',
+    ['react', 'css', 'stuff']
+));
+
 document.addEventListener('DOMContentLoaded', function(){
     const projectlist = document.getElementById('projects')
     projects.forEach(function(project){
