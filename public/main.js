9771e9b27db5d2e287b6c58f6f50915d09b53c58
Tim Kharshan <timkharshan@hotmail.com>
2018-01-01 15:06:31 -0800

    dynamically loading project data

--- public/main.js
+++ public/main.js
@@ -11,21 +11,13 @@ const projects = [];
 
 projects.push( new Project(
     'http://res.cloudinary.com/flyakite/image/upload/v1514414844/search_rifri5.png',
-    ['github.com','linkedin.com'],
-    'rgba(100,100,100,1)',
+    {source:'github.com',live:'heroku.com'},
+    ['rgba(100,100,100,1)'],
     "FlyaKite",
     'React, Rails, and Postgres powered EventBrite clone',
     ['react','css','stuff']
 ));
 
-projects.push(new Project(
-    'http://res.cloudinary.com/flyakite/image/upload/v1514414844/search_rifri5.png',
-    ['github.com', 'linkedin.com'],
-    'rgba(100,100,100,1)',
-    "FlyaKiteee",
-    'React, Rails, and Postgres powered EventBrite clone',
-    ['react', 'css', 'stuff']
-));
 document.addEventListener('DOMContentLoaded', function(){
     const projectlist = document.getElementById('projects')
     projects.forEach(function(project){
@@ -36,9 +28,32 @@ document.addEventListener('DOMContentLoaded', function(){
 
 const createProject = function(parent,newproj){
     const template = document.getElementById('projectTemplate').cloneNode(true);
-    console.log('template',template);
-    console.log('childnode',template.childNodes);
-    console.log('get', template.querySelector('#project-modal'));
+    // console.log('template',template);
+    // console.log('childnode',template.childNodes);
+
+    //setup modal
+    const pointList = template.querySelector('#points');
+    const liveEl = template.querySelector('#live');
+    const sourceEl = template.querySelector('#source');
+    newproj.points.forEach( function(point){
+        const pointEl = document.createElement('p');
+        pointEl.innerText=point;
+        pointList.appendChild(pointEl);
+    });
+    liveEl.href = newproj.links.live;
+    sourceEl.href = newproj.links.source;
+
+    //setup image and header
+    const img = template.querySelector('img');
+    const titleEl = template.querySelector('.title');
+    const headerEl = template.querySelector('.header');
+
+    img.src = newproj.img;
+    titleEl.innerText = newproj.title;
+    headerEl.innerText = newproj.header;
+
+    //change id, take off display:none class and append
+
     template.id = newproj.title;
     template.className = '';
     parent.appendChild(template);
