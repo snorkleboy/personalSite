7cd171046412114f751466c7678fc94eb99ad2b5
Tim Kharshan <timkharshan@hotmail.com>
2018-01-14 20:37:14 -0800

    server fix

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+
+let port = process.env.PORT || 3000;
 // Define the port to run on
 app.set('port', 3000);
 
@@ -8,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Listen for requests
 var server = app.listen(app.get('port'), function() {
-  var port = server.address().port;
+  port = server.address().port;
   console.log('listening on ' + port);
 });
 
