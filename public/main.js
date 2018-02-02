9bb8026e428b2fef946434f0341d3844b1a8ef81
Artem Kharshan <timkharshan@hotmail.com>
2018-02-01 19:25:06 -0800

    git ssh test

--- public/main.js
+++ public/main.js
@@ -1,3 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const projectlist = document.getElementById('projects')
+    projects.forEach(function (project) {
+        createProject(projectlist, project);
+    });
+    activeLinker();
+
+});
+
 const Project = function(img, links, colors, title, header, points){
     this.img = img;
     this.links = links;
@@ -11,7 +20,7 @@ const projects = [];
 
 projects.push(new Project(
     'http://res.cloudinary.com/flyakite/image/upload/q_60/v1514414844/search_rifri5.png',
-    { source: 'http://www.github.com', live: 'heroku.com' },
+    { source: 'https://github.com/snorkleboy/FlyAKite', live: 'http://flyakite.artemkharshan.com' },
     ['rgba(100,100,100,1)'],
     "FlyaKite",
     'React, Rails, and Postgres powered EventBrite clone',
@@ -24,7 +33,7 @@ projects.push(new Project(
 
 projects.push(new Project(
     'http://res.cloudinary.com/flyakite/image/upload/q_60/v1515983798/tor-logo-large-89ac1c118b86d69953fff1ab31128550fcce9a74162e9b3fb7d0fc4bfd83e1ed_u2mbbn.png',
-    { source: 'http://www.github.com', live: 'heroku.com' },
+    { source: 'https://github.com/reidjs/onionup', live: 'http://onionup.artemkharshan.com' },
     ['rgba(100,100,100,1)'],
     "Onionup",
     'A Vue/Vuex App for pinging Sites on the Onion Network',
@@ -37,7 +46,7 @@ projects.push(new Project(
 
 projects.push(new Project(
     'http://res.cloudinary.com/flyakite/image/upload/c_scale,q_56,w_203/v1515907377/download_1_fl6gow_kltvt8.png',
-    { source: 'http://www.github.com', live: 'heroku.com' },
+    { source: 'https://github.com/snorkleboy/Compression-visualizer', live: 'http://quadtree.artemkharshan.com' },
     ['rgba(100,100,100,1)'],
     "Compression Visualizer",
     'A javascript Quadtree Compression Algorithm',
@@ -51,22 +60,37 @@ parameters and click on nodes as the algorithm runs`,
 
 projects.push(new Project(
     'https://www.tcpi.com/wp-content/themes/Divi-child/images/tcp-logo.svg',
-    { source: 'http://www.github.com', live: 'heroku.com' },
+    { source: 'https://github.com/snorkleboy/TCPchat', live: 'http://chat.artemkharshan.com' },
     ['rgba(100,100,100,1)'],
     "TCP chat",
     '-WIP- A CLI and Browser Instant Chat App',
     ['Designed a custom server using Ruby TCP Sockets', 'Made a CLI instant chat app with Ruby Threads and TCP sockets', 'Will make a HTML WebSocket based single page app instant chat that I will hook up with the TCP chat. ']
 ));
+
+const projTemplate = `
+    <div id='project-modal' class='project-modal'>
+        <ul id='points'>
+
+        </ul>
+        <div class='modal-a-holder'>
+            <a id='live' href=''>live</a>
+            <a id='source' href=''>source</a>
+        </div>
+
+    </div>
+    <div class='project-holder'>
+            <img src='' />
+        <span class='title-header'>
+            <h1 class='title'></h1>
+            <h2 class='header'></h2>
+        </span>
+    </div>
 `
-
-
-`
-
 
 const createProject = function(parent,newproj){
-    const template = document.getElementById('projectTemplate').cloneNode(true);
-    // console.log('template',template);
-    // console.log('childnode',template.childNodes);
+    // create element from template
+    const template = document.createElement('li');
+    template.innerHTML = projTemplate;
 
     //setup modal
     const pointList = template.querySelector('#points');
@@ -89,16 +113,13 @@ const createProject = function(parent,newproj){
     titleEl.innerText = newproj.title;
     headerEl.innerText = newproj.header;
 
-    //change id, take off display:none class and append
+    //change id and append
 
     template.id = newproj.title;
-    template.className = '';
+    // template.className = '';
     parent.appendChild(template);
 }
 const activeLinker = function(){
-
-
-
     let tick = false;
     let current = null;
     const seperators = document.querySelectorAll('.seperator')
@@ -151,11 +172,3 @@ const activeLinker = function(){
 };
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    const projectlist = document.getElementById('projects')
-    projects.forEach(function (project) {
-        createProject(projectlist, project);
-    });
-    activeLinker();
-
-});
